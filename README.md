05b3eaad6835018b3eb3239f3e344815b6547098
Anam258 <anamanwer268@gmail.com>
2024-06-16 00:20:36 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Anam258_Asynchronous-Assignment
+ This is a Onsite Class (Thursday 9:00 am to 12:00 pm) Assignment given by Miss Manhil Jameel.
---
+++ main.ts
@@ -0,0 +1,86 @@
+//Question # 1
+async function fetchGreeting() {
+    setTimeout(() => {
+        console.log(`Assalam O Alaikum, "Hajj Mubarak"`);
+    }, 2000); // 2 second delay
+}
+
+fetchGreeting(); //calling function
+
+//Question # 2
+function simulateTask() {
+    console.log("Task started");
+    setTimeout(() => {
+        console.log("Task completed");
+    }, 1000); // 1 second delay
+}
+
+simulateTask(); //calling function
+
+//Question # 3
+function fetchData() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Data fetched successfully!");
+        }, 1000); // 1 second delay
+    });
+}
+fetchData().then((message) => {
+    console.log(message); 
+});
+// 
+//Qustion # 4
+function fetchWithError(): Promise<string> {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const success = Math.random() > 0.5; // Randomly decide success or failure
+            if (success) {
+                resolve("Data fetched successfully!");
+            } else {
+                reject("Data fetch failed!");
+            }
+        }, 1000); // 1 second delay
+    });
+}
+
+fetchWithError()
+    .then((message: string) => {
+        console.log(message); 
+    })
+    .catch((error: string) => {
+        console.error(error); 
+    });
+
+//Question # 5
+function fetchedData(): Promise<string> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Data fetched successfully!");
+        }, 1000); // 1 second delay
+    });
+}
+
+function processData(data: string): Promise<string> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Processing result: ${data}`);
+        }, 1000); // 1 second delay
+    });
+}
+
+function executeSequentially() {
+    fetchedData()
+        .then((data) => {
+            console.log(data);
+            return processData(data);
+        })
+        .then((processedData) => {
+            console.log(processedData);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+
+executeSequentially();
